efb24dc15791d8ae8f0636dcac4e8de168292d29
katiereid14 <kreid@bowdoin.edu>
2022-11-10 13:02:13 -0500

    corrected lists

--- list1.js
+++ list1.js
@@ -1,7 +1,7 @@
 var list1 = [
   {
     "sentence": "You drop a bowling ball on a porcelain figure",
-    "pic _name": "figureN.jpg",
+    "pic_name": "figureN.jpg",
     "correct": "S",
     "language": "english",
     "condition": "intact",
@@ -9,7 +9,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a bowling ball on a piece of cake",
-    "pic _name": "cakeN.jpg",
+    "pic_name": "cakeN.jpg",
     "correct": "S",
     "language": "english",
     "condition": "intact",
@@ -17,7 +17,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a bowling ball on an empty can of beer",
-    "pic _name": "canN.jpg",
+    "pic_name": "canN.jpg",
     "correct": "S",
     "language": "english",
     "condition": "intact",
@@ -25,7 +25,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a bowling ball on a cracker",
-    "pic _name": "crackerN.jpg",
+    "pic_name": "crackerN.jpg",
     "correct": "S",
     "language": "english",
     "condition": "intact",
@@ -33,7 +33,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a bowling ball on a wineglass",
-    "pic _name": "wineglassN.jpg",
+    "pic_name": "wineglassN.jpg",
     "correct": "S",
     "language": "english",
     "condition": "intact",
@@ -41,7 +41,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a bowling ball on an egg",
-    "pic _name": "eggN.jpg",
+    "pic_name": "eggN.jpg",
     "correct": "S",
     "language": "english",
     "condition": "intact",
@@ -49,7 +49,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a bowling ball on an egg flan",
-    "pic _name": "eggflanS.jpg",
+    "pic_name": "eggflanS.jpg",
     "correct": "S",
     "language": "english",
     "condition": "smashed",
@@ -57,7 +57,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a bowling ball on a blackberry",
-    "pic _name": "blackberryS.jpg",
+    "pic_name": "blackberryS.jpg",
     "correct": "S",
     "language": "english",
     "condition": "smashed",
@@ -65,7 +65,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a bowling ball on glasses",
-    "pic _name": "glassesS.jpg",
+    "pic_name": "glassesS.jpg",
     "correct": "S",
     "language": "english",
     "condition": "smashed",
@@ -73,7 +73,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a bowling ball on a hat",
-    "pic _name": "hatS.jpg",
+    "pic_name": "hatS.jpg",
     "correct": "S",
     "language": "english",
     "condition": "smashed",
@@ -81,7 +81,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a bowling ball on a chocolate ice-cream",
-    "pic _name": "icecreamS.jpg",
+    "pic_name": "icecreamS.jpg",
     "correct": "S",
     "language": "english",
     "condition": "smashed",
@@ -89,7 +89,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a bowling ball on an iPAD",
-    "pic _name": "iPadS.jpg",
+    "pic_name": "iPadS.jpg",
     "correct": "S",
     "language": "english",
     "condition": "smashed",
@@ -97,7 +97,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras una bola de boliche a un iPhone",
-    "pic _name": "iphoneN.jpg",
+    "pic_name": "iphoneN.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "intact",
@@ -105,7 +105,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras una bola de boliche a un foco",
-    "pic _name": "lampN.jpg",
+    "pic_name": "lampN.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "intact",
@@ -113,7 +113,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras una bola de boliche a un espejo",
-    "pic _name": "mirrorN.jpg",
+    "pic_name": "mirrorN.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "intact",
@@ -121,7 +121,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras una bola de boliche a un muffin de chocolate",
-    "pic _name": "muffinN.jpg",
+    "pic_name": "muffinN.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "intact",
@@ -129,7 +129,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras una bola de boliche a una rosquilla",
-    "pic _name": "donutN.jpg",
+    "pic_name": "donutN.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "intact",
@@ -137,7 +137,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras una bola de boliche pastel de arroz",
-    "pic _name": "tortitaN.jpg",
+    "pic_name": "tortitaN.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "intact",
@@ -145,7 +145,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras una bola de boliche plato de cer&aacutemica",
-    "pic _name": "plateS.jpg",
+    "pic_name": "plateS.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "smashed",
@@ -153,7 +153,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras una bola de boliche a una fresa",
-    "pic _name": "strawberryS.jpg",
+    "pic_name": "strawberryS.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "smashed",
@@ -161,7 +161,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a bowling ball on a piece of sushi",
-    "pic _name": "sushiS.jpg",
+    "pic_name": "sushiS.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "smashed",
@@ -169,7 +169,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras una bola de boliche a una teja",
-    "pic _name": "tileS.jpg",
+    "pic_name": "tileS.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "smashed",
@@ -177,7 +177,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras una bola de boliche a un tomate",
-    "pic _name": "tomatoS.jpg",
+    "pic_name": "tomatoS.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "smashed",
@@ -185,7 +185,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras una bola de boliche a una taza de papel",
-    "pic _name": "papercupS.jpg",
+    "pic_name": "papercupS.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "smashed",
@@ -193,7 +193,7 @@ var list1 = [
   },
   {
     "sentence": "You see how a musician is playing a guitar",
-    "pic _name": "guitar.jpg",
+    "pic_name": "guitar.jpg",
     "correct": "S",
     "language": "english",
     "condition": "filler",
@@ -201,7 +201,7 @@ var list1 = [
   },
   {
     "sentence": "You see how a cat is drinking water from a bowl",
-    "pic _name": "bowl.jpg",
+    "pic_name": "bowl.jpg",
     "correct": "S",
     "language": "english",
     "condition": "filler",
@@ -209,7 +209,7 @@ var list1 = [
   },
   {
     "sentence": "You see how an old man is hammering a nail",
-    "pic _name": "nail.jpg",
+    "pic_name": "nail.jpg",
     "correct": "S",
     "language": "english",
     "condition": "filler",
@@ -217,7 +217,7 @@ var list1 = [
   },
   {
     "sentence": "You see how a boy is peeling a banana",
-    "pic _name": "banana.jpg",
+    "pic_name": "banana.jpg",
     "correct": "S",
     "language": "english",
     "condition": "filler",
@@ -225,7 +225,7 @@ var list1 = [
   },
   {
     "sentence": "You see how an elephant is eating a watermelon",
-    "pic _name": "watermelon.jpg",
+    "pic_name": "watermelon.jpg",
     "correct": "S",
     "language": "english",
     "condition": "filler",
@@ -233,7 +233,7 @@ var list1 = [
   },
   {
     "sentence": "You see how a postman is driving a car",
-    "pic _name": "car.jpg",
+    "pic_name": "car.jpg",
     "correct": "S",
     "language": "english",
     "condition": "filler",
@@ -241,7 +241,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a toothbrush on the sponge",
-    "pic _name": "spacesuit.jpg",
+    "pic_name": "spacesuit.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -249,7 +249,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a fork on the plate",
-    "pic _name": "tennisball.jpg",
+    "pic_name": "tennisball.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -257,7 +257,7 @@ var list1 = [
   },
   {
     "sentence": "You see how an astronaut is putting on a space suit",
-    "pic _name": "ladder.jpg",
+    "pic_name": "ladder.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -265,7 +265,7 @@ var list1 = [
   },
   {
     "sentence": "You see how a puppy is playing with a ball",
-    "pic _name": "bottlecap.jpg",
+    "pic_name": "bottlecap.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -273,7 +273,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a cabbage on the table",
-    "pic _name": "bow.jpg",
+    "pic_name": "bow.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -281,7 +281,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a keyboard on the bed",
-    "pic _name": "CD.jpg",
+    "pic_name": "CD.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -289,7 +289,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a watch on the laptop",
-    "pic _name": "perfume.jpg",
+    "pic_name": "perfume.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -297,7 +297,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a cushion on the magazine",
-    "pic _name": "button.jpg",
+    "pic_name": "button.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -305,7 +305,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a remote on the rug",
-    "pic _name": "chocolate.jpg",
+    "pic_name": "chocolate.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -313,7 +313,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a pencil on the shoe",
-    "pic _name": "clock.jpg",
+    "pic_name": "clock.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -321,7 +321,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a ring on the newspaper",
-    "pic _name": "coconut.jpg",
+    "pic_name": "coconut.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -329,7 +329,7 @@ var list1 = [
   },
   {
     "sentence": "You drop a needle on the mouse pad",
-    "pic _name": "couch.jpg",
+    "pic_name": "couch.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -337,7 +337,7 @@ var list1 = [
   },
   {
     "sentence": "You see how a man is shaving his beard",
-    "pic _name": "cup.jpg",
+    "pic_name": "cup.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -345,7 +345,7 @@ var list1 = [
   },
   {
     "sentence": "You see how a woman is taking money out of a wallet",
-    "pic _name": "doll.jpg",
+    "pic_name": "doll.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -353,7 +353,7 @@ var list1 = [
   },
   {
     "sentence": "You see how a donkey is drinking water.",
-    "pic _name": "headphones.jpg",
+    "pic_name": "headphones.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -361,7 +361,7 @@ var list1 = [
   },
   {
     "sentence": "You see how a man is boiling potatoes.",
-    "pic _name": "matchesbox.jpg",
+    "pic_name": "matchesbox.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -369,7 +369,7 @@ var list1 = [
   },
   {
     "sentence": "You see how a dentist is drinking a can of cola",
-    "pic _name": "pants.jpg",
+    "pic_name": "pants.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -377,7 +377,7 @@ var list1 = [
   },
   {
     "sentence": "You see how a nurse is taking a syringe",
-    "pic _name": "dustbin.jpg",
+    "pic_name": "dustbin.jpg",
     "correct": "N",
     "language": "english",
     "condition": "filler",
@@ -385,7 +385,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un hombre est&aacute jugando con un gato",
-    "pic _name": "cat.jpg",
+    "pic_name": "cat.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "filler",
@@ -393,7 +393,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un ladr&oacuten est&aacute robando una billetera",
-    "pic _name": "wallet.jpg",
+    "pic_name": "wallet.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "filler",
@@ -401,7 +401,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un b&uacuteho est&aacute reposando en una rama",
-    "pic _name": "owl.jpg",
+    "pic_name": "owl.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "filler",
@@ -409,7 +409,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras un term&oacutemetro a una caja",
-    "pic _name": "box.jpg",
+    "pic_name": "box.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "filler",
@@ -417,7 +417,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un cocinero est&aacute agarrando un cuchillo",
-    "pic _name": "knife.jpg",
+    "pic_name": "knife.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "filler",
@@ -425,7 +425,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un fot&oacutegrafo est&aacute tomando una foto de un le&oacuten.",
-    "pic _name": "lion.jpg",
+    "pic_name": "lion.jpg",
     "correct": "S",
     "language": "spanish",
     "condition": "filler",
@@ -433,7 +433,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un doctor est&aacute agarrando un estetoscopio",
-    "pic _name": "pencil.jpg",
+    "pic_name": "pencil.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -441,7 +441,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un estudiante est&aacute escribiendo en la pizarra",
-    "pic _name": "pillow.jpg",
+    "pic_name": "pillow.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -449,7 +449,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un actriz est&aacute tomando el micr&oacutefono",
-    "pic _name": "purse.jpg",
+    "pic_name": "purse.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -457,7 +457,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un gatito est&aacute jugando en la caja de arena",
-    "pic _name": "ring.jpg",
+    "pic_name": "ring.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -465,7 +465,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute tiras una toalla en la televisi&oacuten.",
-    "pic _name": "dollar.jpg",
+    "pic_name": "dollar.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -473,7 +473,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como una ni&ntildea adolescente est&aacute tocando el piano",
-    "pic _name": "rubberduck.jpg",
+    "pic_name": "rubberduck.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -481,7 +481,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un caballo est&aacute masticando el heno",
-    "pic _name": "shirt.jpg",
+    "pic_name": "shirt.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -489,7 +489,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como el rat&oacuten est&aacute comiendo queso.",
-    "pic _name": "soap.jpg",
+    "pic_name": "soap.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -497,7 +497,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un panda est&aacute descansando en un &aacuterbol",
-    "pic _name": "sock.jpg",
+    "pic_name": "sock.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -505,7 +505,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un loro est&aacute comiendo una manzana",
-    "pic _name": "spoon.jpg",
+    "pic_name": "spoon.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -513,7 +513,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como una mariposa se est&aacute sentando en una flor.",
-    "pic _name": "teddybear.jpg",
+    "pic_name": "teddybear.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -521,7 +521,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como una peluquera est&aacute agarrando las tijeras",
-    "pic _name": "tissuebox.jpg",
+    "pic_name": "tissuebox.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -529,7 +529,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un piloto est&aacute leyendo un libro.",
-    "pic _name": "vase.jpg",
+    "pic_name": "vase.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -537,7 +537,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un polic&iacutea est&aacute hablando por el tel&eacutefono",
-    "pic _name": "watch.jpg",
+    "pic_name": "watch.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -545,7 +545,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como un presidente est&aacute escribiendo en el cuaderno",
-    "pic _name": "ceramicplate.jpg",
+    "pic_name": "ceramicplate.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -553,7 +553,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como una mujer est&aacute pagando con una tarjeta de d&eacutebito",
-    "pic _name": "cigaretteB.jpg",
+    "pic_name": "cigaretteB.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -561,7 +561,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como una jirafa est&aacute jugando con una cebra.",
-    "pic _name": "mangoN.jpg",
+    "pic_name": "mangoN.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
@@ -569,7 +569,7 @@ var list1 = [
   },
   {
     "sentence": "T&uacute ves como una cebra est&aacute jugando con un caballo",
-    "pic _name": "pateS.jpg",
+    "pic_name": "pateS.jpg",
     "correct": "N",
     "language": "spanish",
     "condition": "filler",
