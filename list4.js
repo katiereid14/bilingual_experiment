0cd82f2077d4b2a6b5e635f95dc8624c15c33096
katiereid14 <kreid@bowdoin.edu>
2022-11-09 23:34:20 -0500

    almost done

---
+++ list4.js
@@ -0,0 +1,578 @@
+var list4 = [
+    {
+      "sentence": "You drop a bowling ball on a porcelain figure",
+      "pic_name": "figureS.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "smashed",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a bowling ball on a piece of cake",
+      "pic_name": "cakeS.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "smashed",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a bowling ball on an empty can of beer",
+      "pic_name": "canS.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "smashed",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a bowling ball on a cracker",
+      "pic_name": "crackerS.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "smashed",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a bowling ball on a wine glass",
+      "pic_name": "wineglassS.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "smashed",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a bowling ball on an egg",
+      "pic_name": "eggS.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "smashed",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una bola de boliche a un flan",
+      "pic_name": "eggflanN.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "intact",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una bola de boliche a una morra",
+      "pic_name": "blackberryN.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "intact",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una bola de boliche a unas gafas",
+      "pic_name": "glassesN.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "intact",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una bola de boliche a una gorra",
+      "pic_name": "hatN.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "intact",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una bola de boliche a un helado de chocolate",
+      "pic_name": "icecreamN.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "intact",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una bola de boliche a un iPad",
+      "pic_name": "iPadN.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "intact",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una bola de boliche a un iPhone",
+      "pic_name": "iphoneS.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "smashed",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una bola de boliche a un foco",
+      "pic_name": "lampS.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "smashed",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una bola de boliche a un espejo",
+      "pic_name": "mirrorS.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "smashed",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una bola de boliche a un muffin de chocolate",
+      "pic_name": "muffinS.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "smashed",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una bola de boliche a una rosquilla",
+      "pic_name": "donutS.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "smashed",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una bola de boliche pastel de arroz",
+      "pic_name": "tortitaS.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "smashed",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a bowling ball on a ceramic plate",
+      "pic_name": "plateN.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "intact",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a bowling ball on a strawberry",
+      "pic_name": "strawberryN.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "intact",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a bowling ball on a piece of sushi",
+      "pic_name": "sushiN.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "intact",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a bowling ball on a tile",
+      "pic_name": "tileN.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "intact",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a bowling ball on a tomato",
+      "pic_name": "tomatoN.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "intact",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a bowling ball on a paper cup",
+      "pic_name": "papercupN.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "intact",
+      "list": 4
+    },
+    {
+      "sentence": "You see how a musician is playing a guitar",
+      "pic_name": "guitar.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how a cat is drinking water from a bowl",
+      "pic_name": "bowl.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how an old man is hammering a nail",
+      "pic_name": "nail.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how a boy is peeling a banana",
+      "pic_name": "banana.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how an elephant is eating a watermelon",
+      "pic_name": "watermelon.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how a postman is driving a car",
+      "pic_name": "car.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a toothbrush on the sponge",
+      "pic_name": "spacesuit.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a fork on the plate",
+      "pic_name": "tennisball.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how an astronaut is putting on a space suit",
+      "pic_name": "ladder.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how a puppy is playing with a ball",
+      "pic_name": "bottlecap.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a cabbage on the table",
+      "pic_name": "bow.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a keyboard on the bed",
+      "pic_name": "CD.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a watch on the laptop",
+      "pic_name": "perfume.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a cushion on the magazine",
+      "pic_name": "button.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a remote on the rug",
+      "pic_name": "chocolate.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a pencil on the shoe",
+      "pic_name": "clock.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a ring on the newspaper",
+      "pic_name": "coconut.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You drop a needle on the mouse pad",
+      "pic_name": "couch.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how a man is shaving his beard",
+      "pic_name": "cup.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how a woman is taking money out of a wallet",
+      "pic_name": "doll.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how a donkey is drinking water.",
+      "pic_name": "headphones.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how a man is boiling potatoes.",
+      "pic_name": "matchesbox.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how a dentist is drinking a can of cola",
+      "pic_name": "pants.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "You see how a nurse is taking a syringe",
+      "pic_name": "dustbin.jpg",
+      "correct": "N",
+      "language": "english",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un hombre est&aacute jugando con un gato",
+      "pic_name": "cat.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un ladr&oacuten est&aacute robando una billetera",
+      "pic_name": "wallet.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un b&uacuteho est&aacute reposando en una rama",
+      "pic_name": "owl.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras un term&oacutemetro a una caja",
+      "pic_name": "box.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un cocinero est&aacute agarrando un cuchillo",
+      "pic_name": "knife.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un fot&oacutegrafo est&aacute tomando una foto de un le&oacuten.",
+      "pic_name": "lion.jpg",
+      "correct": "S",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un doctor est&aacute agarrando un estetoscopio",
+      "pic_name": "pencil.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un estudiante est&aacute escribiendo en la pizarra",
+      "pic_name": "pillow.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un actriz est&aacute tomando el micr&oacutefono",
+      "pic_name": "purse.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un gatito est&aacute jugando en la caja de arena",
+      "pic_name": "ring.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute tiras una toalla en la televisi&oacuten.",
+      "pic_name": "dollar.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como una ni&ntildea adolescente est&aacute tocando el piano",
+      "pic_name": "rubberduck.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un caballo est&aacute masticando el heno",
+      "pic_name": "shirt.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como el rat&oacuten est&aacute comiendo queso.",
+      "pic_name": "soap.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un panda est&aacute descansando en un &aacuterbol",
+      "pic_name": "sock.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un loro est&aacute comiendo una manzana",
+      "pic_name": "spoon.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como una mariposa se est&aacute sentando en una flor.",
+      "pic_name": "teddybear.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como una peluquera est&aacute agarrando las tijeras",
+      "pic_name": "tissuebox.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un piloto est&aacute leyendo un libro.",
+      "pic_name": "vase.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un polic&iacutea est&aacute hablando por el tel&eacutefono",
+      "pic_name": "watch.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como un presidente est&aacute escribiendo en el cuaderno",
+      "pic_name": "ceramicplate.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como una mujer est&aacute pagando con una tarjeta de d&eacutebito",
+      "pic_name": "cigaretteB.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como una jirafa est&aacute jugando con una cebra.",
+      "pic_name": "mangoN.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    },
+    {
+      "sentence": "T&uacute ves como una cebra est&aacute jugando con un caballo",
+      "pic_name": "pateS.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "filler",
+      "list": 4
+    }
+  ]
