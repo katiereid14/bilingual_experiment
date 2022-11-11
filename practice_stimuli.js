f4ae2ae5958097a8f92c602784b2f734736d7531
Bryam1 <bbarbech@dhcp-222-173.student-wlan.bowdoin.edu>
2022-11-11 15:10:27 -0500

    update2

---
+++ practice_stimuli.js
@@ -0,0 +1,30 @@
+var practicestimuli = [
+    {
+      "sentence": "You drop a bowling ball on a teacup",
+      "pic_name": "cupB.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "practice"
+    },
+    {
+      "sentence": "You drop a bowling ball on a cigarette",
+      "pic_name": "cigaretteB.jpg",
+      "correct": "S",
+      "language": "english",
+      "condition": "practice"
+    },
+    {
+      "sentence": "Tu tiras una bola de boliche a una botella",
+      "pic_name": "bookB.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "practice"
+    },
+    {
+      "sentence": "Tu tiras una bola de boliche a un vaso",
+      "pic_name": "bananaB.jpg",
+      "correct": "N",
+      "language": "spanish",
+      "condition": "practice"
+    }
+  ]
